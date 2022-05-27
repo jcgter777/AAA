b2349bd942d4b0686b871d388bdbf4c201988c3c
Joshua Chen <jcgter777@gmail.com>
2022-05-27 09:21:38 -0400

    First 2 cards made

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+var current = 0
+const divs = [
+    "intro",
+    "intro2"
+]
+function next() {
+    if (current == divs.length-1)
+        return
+    document.getElementById(divs[current]).style.display = "none"
+    current++
+    document.getElementById(divs[current]).style.display = "block"
+}
