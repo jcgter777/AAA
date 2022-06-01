1f361a71f19c3c8cb36e9c23460e97a1ec2a39ea
Joshua Chen <jcgter777@gmail.com>
2022-06-01 11:22:37 -0400

    Added new styling, error and success messages for elev puzzle

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ var current = 0
 const divs = [
     "intro1",
     "intro2",
-    "puzzle1.1",
+    "puzzle1",
+    "open1",
 ]
 function next() {
     if (current == divs.length-1)
@@ -10,4 +11,35 @@ function next() {
     document.getElementById(divs[current]).style.display = "none"
     current++
     document.getElementById(divs[current]).style.display = "block"
+
+    let div = document.getElementById(divs[current])
+    if (div.classList.contains("locked")) {
+        document.getElementById("continue").style.display = "none"
+    }
+}
+
+function elev1() {
+    elevFail()
+}
+
+function elev2() {
+    if (document.getElementById("elevSucc").style.display === "block")
+        return
+    document.getElementById("continue").style.display = "block"
+    document.getElementById("elevSucc").style.display = "block"
+    elevLock()
+}
+
+function elev3() {
+    elevFail()
+}
+
+function elevFail() {
+    let bar = document.getElementById("elevError")
+    if (bar != null)
+        bar.style.display = "block"
+}
+
+function elevLock() {
+    document.getElementById("elevError").remove()
 }
